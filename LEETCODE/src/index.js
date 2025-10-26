482f57d268c8d352497103e2c98a9401b874b6ca
Raj Kumar <imraaj1026@gmail.com>
2025-10-26 10:29:12 +0530

    almost done leetcode backend

--- LEETCODE/src/index.js
+++ LEETCODE/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.routes.js';
 import problemRoutes from './routes/problem.routes.js'
+import executeCodeRoutes from './routes/executeCode.routes.js';
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/problems', problemRoutes);
+app.use('/api/v1/execute-code', executeCodeRoutes);
 
 
 app.listen(PORT,()=>{
