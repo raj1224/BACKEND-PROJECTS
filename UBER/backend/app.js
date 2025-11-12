e8d2ea2ff99b62e3b16cb403f9afba60851b1716
Raj Kumar <imraaj1026@gmail.com>
2025-11-12 11:14:07 +0530

    database connected successfully

--- UBER/backend/app.js
+++ UBER/backend/app.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import { connectDB } from './config/db.config.js';
+
+
+dotenv.config();
+connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
--- UBER/backend/server.js
+++ UBER/backend/server.js
@@ -1,5 +1,10 @@
 import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import app from './app.js';
+
 
 const PORT = process.env.PORT || 8080;
 
