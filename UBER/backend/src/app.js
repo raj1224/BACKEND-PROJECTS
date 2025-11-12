3d6164c4f7193471e021719031a2e714138297ff
Raj Kumar <imraaj1026@gmail.com>
2025-11-12 10:51:16 +0530

    uber project initailized

---
+++ UBER/backend/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+export default app;
