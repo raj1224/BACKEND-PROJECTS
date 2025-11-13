ecc048fc7316f81ef8a458a91cd33060ed6ab0d4
Raj Kumar <imraaj1026@gmail.com>
2025-11-13 12:02:39 +0530

    frontend is ongoing

--- UBER/frontend/src/App.jsx
+++ UBER/frontend/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserRegister from './pages/UserRegister'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainRegister from './pages/CaptainRegister'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/user-login' element={<UserLogin/>} />
+        <Route path='/user-register' element={<UserRegister/>} />
+        <Route path='/captain-login' element={<CaptainLogin/>} />
+        <Route path='/captain-register' element={<CaptainRegister/>} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ UBER/frontend/src/pages/CaptainRegister.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CaptainRegister() {
+  return (
+    <div>CaptainRegister</div>
+  )
+}
+
+export default CaptainRegister
